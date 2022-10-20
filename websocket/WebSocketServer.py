a22d6ab10468e4533e460b7e57fb2153cec69246
wouter.devriese@luca-arts.be <wouter.devriese@luca-arts.be>
2022-10-20 13:53:05 +0200

    updated for hello message

--- websocket/WebSocketServer.py
+++ websocket/WebSocketServer.py
@@ -16,13 +16,17 @@ async def ws_handler(websocket, params):
         message_dict = ast.literal_eval(message)
         for key, value in message_dict.items():
             match key:
+                case "connectMessage":
+                    print("connected with {}".format(value))
+                case "_time":
+                    print("connection time is {}".format(value))
                 case "caregiver_rating":
                     print("care giver got a rating of {}".format(value))
                 case default:
                     print("no know command: {}".format(key))
 
 
-async def startWsServer(params: list[dict] = [{"i": "test"}], ip: str = 'localhost', port: int = 8081):
+async def startWsServer(params: list[dict] = [{"i": "test"}], ip: str = 'localhost', port: int = 8080):
     print("starting websocket server")
     async with websockets.serve(functools.partial(ws_handler, params=params), ip, port) as ws:
         await asyncio.Future()  # run forever
@@ -33,7 +37,7 @@ if __name__ == '__main__':
         {"id": "123"},
         {"contingency": "20"}
     ]
-    asyncio.run(startWsServer(params=websocket_data))
+    asyncio.run(startWsServer(params=websocket_data, ip='10.46.235.49'))
     # loop = asyncio.get_event_loop()
     # task = loop.create_task(startWsServer())
     # try:
